2c64a6dc43c56dd813eed193433e7bec9a4384c1
lloydttt <126597991+lloydttt@users.noreply.github.com>
2024-03-19 23:51:52 +0800

    open_loop_v_control_v2

--- include/open_v_control.h
+++ include/open_v_control.h
@@ -2,24 +2,33 @@
 #define OPEN_V_CONTROL
 
 #include "Ndef_Motor.h"
+#include <cmath>
 
+#define PI 3.1415926
+
+float mach_angle=0, elec_angle=0;
+float u_d=0, u_q=0, u_alpha=0, u_beta=0, u_a=0, u_b=0, u_c=0, pwm_a=0, pwm_b=0, pwm_c=0;
+float timestamp=0, zero_angle=0;
 
 // 相关参数定义
 
-
+#define _constrain(amt,low,high) ((amt)<(low)?(low):((amt)>(high)?(high):(amt)))
 
 // cv使用宏定义的限制函数增快运行速度 使用问号表达式
 
 MOTOR motor = {
-    12.5f,
-    7.0f,
-    5.0f,
-    0.0f
+    12.5f, //额定电压
+    7.0f,  //电机 极对数
+    5.0f,  //目标速度   rad/s
+    0.0f   //目标力矩
 };
 float a = motor.motor_pairs;
 // 初始化电机结构体
 
-
+float Normalize(float angle){
+    float ang_i =  fmod(angle, 2*PI);
+    return ang_i > 0 ? ang_i : ang_i=2*PI;
+}
 
 // 归一化取模，使大于2pi的弧度均转为之内
 
@@ -27,6 +36,9 @@ float a = motor.motor_pairs;
 
 // 电角度求解
 
+void setPWM(float a, float b, float c){
+    
+}
 
 // PWM赋值
 
